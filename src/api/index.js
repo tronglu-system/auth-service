956e678010d5127c3915da36e2523ce273a31bd6
Trong Lu <lvhtrong1793@gmail.com>
2019-10-12 21:05:14 +0700

    Update swagger documentation structure

--- src/api/index.js
+++ src/api/index.js
@@ -1,19 +1,10 @@
 import express from 'express'
-import swaggerUi from 'swagger-ui-express'
-import swaggerSpec from './swagger'
 import mongo from './mongo'
 import routeV1 from './routes/v1'
 
 mongo.setup()
 
 const router = express.Router()
-
-router.get('/docs.json', function (req, res) {
-  res.setHeader('Content-Type', 'application/json')
-  res.send(swaggerSpec)
-})
-
-router.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 router.get('/status', function (req, res) {
   res.send('OK')
--- src/api/routes/v1/index.js
+++ src/api/routes/v1/index.js
@@ -1,5 +1,12 @@
 import express from 'express'
+import path from 'path'
+import swaggerUi from 'swagger-ui-express'
 
 const router = express.Router()
+
+const swaggerSpec = require('yamljs').load(
+  path.join(__dirname, 'swagger.v1.yaml')
+)
+router.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 export default router
